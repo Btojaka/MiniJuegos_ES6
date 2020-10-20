f85244618e8e1b1fff9575f084d3a5198c6688ce
Btojaka <bruthtoj@gmail.com>
2020-10-20 08:07:09 +0200

    Se intenta cambiar la funcion a funcion Array de ES6 pero no se logra buen resultado.

--- mision1.js
+++ mision1.js
@@ -1,24 +1,36 @@
+
 /* Muestra por pantalla un mensaje al usuario para que elija la categoría que quiera
  en la última tirada. Guarda la elección del usuario en una variable y la devuelve.*/
-/*function elegir(){
-    let eleccion = prompt(`Elige la categoría de tu carta 
-    1 = Geografía, 2 = Arte, 3 = Espectáculos,
-    4 = Historia, 5 = Ciencias o 6 = Deporte.`, 3);
-    
-    return eleccion;
-}
-
-switch(elegir()){
-    case 1:
-        alert("Has elegido Geografía");*/
-
-}
-
-
 let elegir = () => prompt(`Elige la categoría de tu carta 
     1 = Geografía, 2 = Arte, 3 = Espectáculos,
     4 = Historia, 5 = Ciencias o 6 = Deporte.`, 3);
     
+switch(elegir()){
+    case 1:
+        alert("Has elegido Geografía");
+    break;
+    case 2:
+        alert("Has elegido Geografía");
+    break;
+    case 3:
+        alert("Has elegido Geografía");
+    break;
+    case 4:
+        alert("Has elegido Geografía");
+    break;
+    case 5:
+        alert("Has elegido Geografía");
+    break;
+    case 6:
+        alert("Has elegido Geografía");
+    break;
+    default:
+        alert("Introduce un número del 1 al 6, vuelve a intentarlo.")
+        elegir();
+        break;
+}
+
+elegir();
 
 
 /*Creará la carta con el parámetro eleccion que será la categoria que eligió el usuario 
@@ -29,4 +41,10 @@ De momento la idea es crrearla a como un formulario*/
         <input></input>
     </form>*/
 
-elegir();
+    /*function elegir(){
+    let eleccion = prompt(`Elige la categoría de tu carta 
+    1 = Geografía, 2 = Arte, 3 = Espectáculos,
+    4 = Historia, 5 = Ciencias o 6 = Deporte.`, 3);
+    
+    return eleccion;
+}*/
